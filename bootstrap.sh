e44d9178f7f6a01c56db7550c01641a5cb2d4b99
Alexey Volvak <daedra@Alexeys-MacBook-Pro.local>
2026-03-04 20:35:49 -0800

    Fix bootstrap: defensively avoid apt if tools are present

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,10 @@ set -e
 echo "Updating system..."
 apt update
 
-echo "Installing dependencies..."
-apt install -y git curl
+echo "Installing dependencies if needed..."
+if ! command -v git &> /dev/null || ! command -v curl &> /dev/null || ! command -v docker &> /dev/null; then
+    apt install -y git curl docker.io docker-compose || echo "Apt install failed, but continuing..."
+fi
 
 echo "Starting docker..."
 systemctl start docker
